919aeccfdc9bf3a3d9cc65c428be04a01fa99ca3
Maxime Dénès <maxime.denes@inria.fr>
2019-07-23 14:47:39 +0200

    Rename `Make` into `_CoqProject`
    This makes it easier to work on GeoCoq using IDEs

--- configure-ci.sh
+++ configure-ci.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
-cp -f Make.in Make
-find . -name "*.v" | grep -v Sandbox | grep -v POF >> Make
+cp -f _CoqProject.in _CoqProject
+find . -name "*.v" | grep -v Sandbox | grep -v POF >> _CoqProject
 
 # We lack a few minutes to be able to build the whole library.
-sed -i.bak '/Ch16_coordinates_with_functions\.v/d'             Make
-sed -i.bak '/Elements\/OriginalProofs\/proposition_30\.v/d'    Make
-sed -i.bak '/Elements\/OriginalProofs\/proposition_44A\.v/d'   Make
-sed -i.bak '/Elements\/OriginalProofs\/proposition_44\.v/d'    Make
-sed -i.bak '/Elements\/OriginalProofs\/proposition_45\.v/d'    Make
-sed -i.bak '/Elements\/OriginalProofs\/book1\.v/d'             Make
-sed -i.bak '/Elements\/Statements\/Book_1\.v/d'                Make
-sed -i.bak '/Elements\/Statements\/Book_3\.v/d'                Make
-sed -i.bak '/main.v/d'                                         Make
+sed -i.bak '/Ch16_coordinates_with_functions\.v/d'             _CoqProject
+sed -i.bak '/Elements\/OriginalProofs\/proposition_30\.v/d'    _CoqProject
+sed -i.bak '/Elements\/OriginalProofs\/proposition_44A\.v/d'   _CoqProject
+sed -i.bak '/Elements\/OriginalProofs\/proposition_44\.v/d'    _CoqProject
+sed -i.bak '/Elements\/OriginalProofs\/proposition_45\.v/d'    _CoqProject
+sed -i.bak '/Elements\/OriginalProofs\/book1\.v/d'             _CoqProject
+sed -i.bak '/Elements\/Statements\/Book_1\.v/d'                _CoqProject
+sed -i.bak '/Elements\/Statements\/Book_3\.v/d'                _CoqProject
+sed -i.bak '/main.v/d'                                         _CoqProject
 
-coq_makefile -f Make -o Makefile
+coq_makefile -f _CoqProject -o Makefile
--- configure-main.sh
+++ configure-main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-cp -f Make.in Make
-find . -name "*.v" | grep -v "Tactics/Coinc/" | grep -v Axioms | grep -v OriginalProofs | grep -v Utils | grep -v Sandbox | grep -v finish | grep -v euclid_to_tarski | grep -v POF >> Make
-coq_makefile -f Make -o Makefile
+cp -f _CoqProject.in _CoqProject
+find . -name "*.v" | grep -v "Tactics/Coinc/" | grep -v Axioms | grep -v OriginalProofs | grep -v Utils | grep -v Sandbox | grep -v finish | grep -v euclid_to_tarski | grep -v POF >> _CoqProject
+coq_makefile -f _CoqProject -o Makefile
