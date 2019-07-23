ffb1df2fb62104fdba103228538be8a593f6d606
Maxime Dénès <maxime.denes@inria.fr>
2019-07-23 14:47:39 +0200

    Rename `Make` into `_CoqProject`
    This makes it easier to work on GeoCoq using IDEs

--- configure-coinc.sh
+++ configure-coinc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cp -f Make.in Make
-find . -name "*.v" | grep "Tactics/Coinc/" >> Make
-find . -name "*.v" | grep Utils >> Make
-coq_makefile -f Make -o Makefile
+cp -f _CoqProject.in _CoqProject
+find . -name "*.v" | grep "Tactics/Coinc/" >> _CoqProject
+find . -name "*.v" | grep Utils >> _CoqProject
+coq_makefile -f _CoqProject -o Makefile
