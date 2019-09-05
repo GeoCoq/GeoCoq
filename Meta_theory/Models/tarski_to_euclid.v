GeoCoq/GeoCoq

commit d91694bcaea6df45f44b2a7a5884a6cedad802cf

synchronize with the SVN repository

Meta_theory/Models/tarski_to_euclid.v
@@ -1,8 +1,8 @@
 Require Import GeoCoq.Tarski_dev.Ch12_parallel.
 Require Import GeoCoq.Axioms.euclidean_axioms.
-Require Export GeoCoq.Axioms.continuity_axioms.
-Require Export GeoCoq.Meta_theory.Continuity.elementary_continuity_props.
-Require Export GeoCoq.Meta_theory.Parallel_postulates.parallel_postulates.
+Require Import GeoCoq.Axioms.continuity_axioms.
+Require Import GeoCoq.Meta_theory.Continuity.elementary_continuity_props.
+Require Import GeoCoq.Meta_theory.Parallel_postulates.parallel_postulates.
 
 Import euclidean_axioms.
 
@@ -13,8 +13,8 @@ Context `{TnEQD:Tarski_neutral_dimensionless_with_decidable_point_equality}.
 
 Definition Tcircle : Type := Tpoint*Tpoint*Tpoint %type.
 
-Definition OnCirc P (C:Tcircle) := 
-  match C with 
+Definition OnCirc P (C:Tcircle) :=
+  match C with
   (X,A,B) => tarski_axioms.Cong X P A B
   end.
 
@@ -22,18 +22,18 @@ Definition CI (J:Tcircle) A C D := J=(A,C,D) /\ C<>D.
 
 Definition InCirc P (J:Tcircle) :=
    match J with
-  (C,A,B) => 
+  (C,A,B) =>
    exists X Y, Definitions.BetS X C Y /\ tarski_axioms.Cong C Y A B /\
                tarski_axioms.Cong C X A B /\ Definitions.BetS X P Y
   end.
 
 Definition OutCirc P (J:Tcircle) :=
    match J with
-  (C,A,B) => 
+  (C,A,B) =>
       exists X, Definitions.BetS C X P /\ tarski_axioms.Cong C X A B
  end.
 
-Lemma on : 
+Lemma on :
  forall A B C D J, CI J A C D /\ OnCirc B J ->
                    tarski_axioms.Cong A B C D.
 Proof.
@@ -46,11 +46,11 @@ congruence.
 Qed.
 
 Lemma inside : forall A B C J P,
-  CI J C A B /\ InCirc P J <-> 
-  exists X Y, CI J C A B /\ 
-   Definitions.BetS X C Y /\ 
-   tarski_axioms.Cong C Y A B /\ 
-   tarski_axioms.Cong C X A B /\ 
+  CI J C A B /\ InCirc P J <->
+  exists X Y, CI J C A B /\
+   Definitions.BetS X C Y /\
+   tarski_axioms.Cong C Y A B /\
+   tarski_axioms.Cong C X A B /\
    Definitions.BetS X P Y.
 Proof.
 intros.
@@ -103,7 +103,7 @@ Lemma bet_cases : forall B C D1 D2,
 Proof.
 intros.
 assert (T:Bet D1 C B \/ Bet D1 B C).
-  apply (l5_3 D1 C B D2);unfold Definitions.BetS in *;spliter;finish.
+  apply (l5_3 D1 C B D2);unfold Definitions.BetS in *;spliter;Between.
  destruct T.
  right.
  unfold Definitions.BetS in *;spliter.
@@ -144,25 +144,18 @@ assert (HColD: Definitions.Col A C C)
 assert (HBet: Bet C C D2) by Between.
 destruct (H C D2 A C C HColD H2 HBet) as [Z1 [Z2 HZ]].
 spliter.
-exists Z1. exists Z2.
-repeat split;try assumption.
-unfold OnCircle, OnCirc in *;eCong.
-unfold OnCircle, OnCirc in *;eCong.
+exists Z1, Z2.
 assert (C<>D2)
  by (unfold Definitions.BetS in *;spliter;auto).
-assert (Z1<>Z2) by auto.
-unfold OnCircle in *.
-intro;treat_equalities;intuition.
-unfold OnCircle in *.
-intro;treat_equalities.
-unfold Definitions.BetS in *;intuition.
+repeat split;try assumption; try (intro; treat_equalities; auto); unfold OnCirc;CongR.
+
 assert (TwoCases:Definitions.BetS D1 B C \/ Definitions.BetS C B D2)
  by (apply bet_cases;auto).
 destruct TwoCases.
 - assert (HColD: Definitions.Col A B B)
    by (unfold Definitions.Col;Between).
 assert (HBet:Bet C B D1)
- by (unfold Definitions.BetS in *;spliter;finish).
+ by (unfold Definitions.BetS in *;spliter;Between).
 destruct (H C D1 A B B HColD H2 HBet)
  as [Z1 [Z2 HZ]].
 exists Z1.
@@ -175,29 +168,28 @@ assert (Z1<>B).
 {
  intro. subst.
  unfold OnCircle in *.
- assert (B=D1) by (apply between_cong with C;finish).
+ assert (B=D1) by (apply between_cong with C;auto).
  subst. intuition.
 }
 assert_diffs.
 assert (B<>Z2).
 {
  intro. subst.
- assert (Bet C Z2 D1) 
-  by (unfold BetS in *;spliter;finish).
+ assert (Bet C Z2 D1)
+  by (unfold BetS in *;spliter;auto).
  unfold OnCircle in *.
  assert (Z2=D1)
-  by (apply between_cong with C;finish).
+  by (apply between_cong with C;auto).
  intuition.
 }
 assert (Definitions.BetS Z1 B Z2)
  by (unfold Definitions.BetS;auto).
-unfold OnCirc;simpl.
-unfold OnCircle in *.
-repeat split; eCong.
+unfold OnCirc.
+repeat split; CongR.
 - assert (HColD: Definitions.Col A B B)
    by (unfold Definitions.Col;Between).
 assert (HBet:Bet C B D2)
- by (unfold Definitions.BetS in *;spliter;finish).
+ by (unfold Definitions.BetS in *;spliter;auto).
 destruct (H C D2 A B B HColD H2 HBet)
  as [Z1 [Z2 HZ]].
 exists Z1.
@@ -210,25 +202,24 @@ assert (Z1<>B).
 {
  intro. subst.
  unfold OnCircle in *.
- assert (B=D2) by (apply between_cong with C;finish).
+ assert (B=D2) by (apply between_cong with C;auto).
  subst. intuition.
 }
 assert_diffs.
 assert (B<>Z2).
 {
  intro. subst.
- assert (Bet C Z2 D2) 
-  by (unfold BetS in *;spliter;finish).
+ assert (Bet C Z2 D2)
+  by (unfold BetS in *;spliter;auto).
  unfold OnCircle in *.
  assert (Z2=D2)
-  by (apply between_cong with C;finish).
+  by (apply between_cong with C;auto).
  intuition.
 }
 assert (Definitions.BetS Z1 B Z2)
  by (unfold Definitions.BetS;auto).
-unfold OnCirc;simpl.
-unfold OnCircle in *.
-repeat split; eCong.
+unfold OnCirc.
+repeat split; CongR.
 Qed.
 
 Lemma circle_circle' :
@@ -257,10 +248,8 @@ spliter.
 unfold OutCirc in *.
 destruct H2 as [X HX].
 spliter.
-assert (OnCircle P D Q)
- by (unfold OnCircle;eCong).
-assert (OnCircle Q D Q)
- by (unfold OnCircle;eCong).
+assert (OnCircle P D Q) by CongR.
+assert (OnCircle Q D Q) by CongR.
 assert (InCircle P C D1).
 {
  unfold InCircle.
@@ -270,23 +259,23 @@ assert (InCircle P C D1).
   by (apply bet_cases;auto).
  destruct TwoCases.
  exists P.
- split; unfold Definitions.BetS in *;spliter; finish.
+ split; Cong; unfold Definitions.BetS in *;spliter; Between.
  apply l5_6 with C P C D2.
  exists P.
- split; unfold Definitions.BetS in *;spliter; finish.
- finish.
- eCong.
+ split; Cong; unfold Definitions.BetS in *;spliter; auto.
+ Cong.
+ apply cong_transitivity with R S; Cong.
 }
 assert (OutCircle Q C D1).
 {
  unfold OutCircle.
  exists X.
- split; unfold Definitions.BetS in *;spliter; eCong.
+ split; [unfold Definitions.BetS in *;spliter; auto|CongR].
 }
 assert (Hex: exists Z : Tpoint, OnCircle Z C D1 /\ OnCircle Z D Q) by eauto.
-destruct Hex as [Z HZ].
+destruct Hex as [Z []].
 exists Z.
-unfold OnCircle in *;spliter;split;eCong.
+split; CongR.
 Qed.
 
 End circle_continuity.
@@ -298,9 +287,9 @@ Context `{TnEQD:Tarski_neutral_dimensionless_with_decidable_point_equality}.
 Global Instance Euclid_neutral_follows_from_Tarski_neutral : euclidean_neutral.
 Proof.
 eapply (Build_euclidean_neutral Tpoint Tcircle tarski_axioms.Cong Tarski_dev.Definitions.BetS (* InCirc OnCirc OutCirc*) tarski_axioms.PA tarski_axioms.PB tarski_axioms.PC CI).
-- intros;apply cong_transitivity with P Q;finish.
-- intro;finish.
-- intro;finish.
+- intros;apply cong_transitivity with P Q;Cong.
+- intro;Cong.
+- intro;Cong.
 - intros;apply (l2_11 A B C a b c); unfold Definitions.BetS in *;intuition.
 - intros.
   destruct (eq_dec_points A B);intuition.
@@ -322,7 +311,7 @@ eapply (Build_euclidean_neutral Tpoint Tcircle tarski_axioms.Cong Tarski_dev.Def
 - intros; unfold Definitions.BetS;
 intro;spliter;treat_equalities;intuition.
 - intros;unfold Definitions.BetS in *;
-intros;spliter;finish.
+intros;spliter;Between.
 - intros;unfold Definitions.BetS in *;
 intros;spliter;try assumption;eBetween.
 - intros;unfold Definitions.BetS in *;
@@ -333,14 +322,14 @@ assert (T3:=eq_dec_points A B).
 assert (T4:=eq_dec_points A C).
 assert (T5:=eq_dec_points B C).
 tauto.
-- intros;intro;treat_equalities;finish.
+- intros;intro;treat_equalities;auto.
 - intros.
 assert (tarski_axioms.Cong C D c d)
  by (apply (five_segment A a B b C c D d);
 unfold Definitions.BetS in *;spliter;auto).
-finish.
+Cong.
 - intros;unfold Definitions.BetS in *;spliter.
-  destruct (inner_pasch A B C P Q H H0) as [X [HXa HXb]]. 
+  destruct (inner_pasch A B C P Q H H0) as [X [HXa HXb]].
   exists X.
   assert_diffs.
   assert (~ Bet A C B) by tauto.
@@ -362,8 +351,8 @@ finish.
   assert (B<>A) by auto.
   assert (Q<>B) by auto.
   assert (~ Bet Q B A) by tauto.
- assert (~ Bet A Q B) by finish.
-  assert (Bet Q C B) by finish.
+ assert (~ Bet A Q B) by Between.
+  assert (Bet Q C B) by Between.
   destruct (outer_pasch Q A C B P H18 H) as [X [HXa HXb]].
   exists X.
    assert_diffs.
@@ -418,10 +407,10 @@ destruct (eq_dec_points B C).
 right;right;left;auto.
 decompose [or] H.
 right;right;right;right;left;auto.
-right;right;right;right;right;finish.
-right;right;right;left;finish.
+right;right;right;right;right;Between.
+right;right;right;left;Between.
 intros.
-decompose [or] H;subst;spliter;finish.
+decompose [or] H;subst;spliter;Between.
 Qed.
 
 Lemma nCol_not_Col : forall A B C,
@@ -442,13 +431,13 @@ intros.
 destruct H as [U [V HUV]];spliter.
 intro.
 unfold Definitions.BetS in *;spliter.
-assert (tarski_axioms.Cong X U X V) by eCong.
-assert (Midpoint X U V) by (split;finish).
+assert (tarski_axioms.Cong X U X V) by CongR.
+assert (Midpoint X U V) by (split;Cong).
 assert (Midpoint X A V).
 apply (cong_col_mid A X V).
 assumption.
 ColR.
-eCong.
+CongR.
 treat_equalities;intuition.
 Qed.
 
@@ -472,23 +461,23 @@ exists X0.
 exists W.
 assert_bets.
 assert_diffs.
-split;finish.
-unfold Definitions.BetS;finish.
-split;finish.
-split;finish.
-unfold Definitions.BetS;finish.
+split.
+unfold Definitions.BetS;Between.
+split;Cong.
+split;Cong.
+unfold Definitions.BetS;Between.
 destruct (segment_construction_3 C0 V V W H1 H0) as [X0 [HX0 HX1]].
 destruct (symmetric_point_construction X0 C0) as [Y0 HY0].
 exists X0.
 exists Y0.
 assert_bets.
 assert_diffs.
-unfold Definitions.BetS;split;finish.
+unfold Definitions.BetS;split;auto.
 split.
-eCong.
+apply cong_transitivity with C0 X0; Cong.
 split.
-eCong.
-split;finish.
+auto.
+split;auto.
 - spliter.
 unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *;spliter.
 inversion H2;subst.
@@ -499,51 +488,48 @@ assert (C0 <> A).
  }
 
 destruct (segment_construction_3 C0 A V W H4 H3) as [X0 [HX0 HX1]].
-destruct (eq_dec_points A X0).
+assert (A <> X0).
  {
-  treat_equalities.
-  assert_diffs.
-  assert (HCong: tarski_axioms.Cong C0 Y C0 X) by eCong.
+  intro.
+  subst.
+  assert (HCong: tarski_axioms.Cong C0 Y C0 X) by CongR.
   unfold Definitions.BetS in *;spliter.
   assert (eq:= between_cong C0 X Y H HCong).
-  subst;intuition.
+  contradiction.
  }
 
 destruct (symmetric_point_construction X0 C0) as [Y0 HY0].
-destruct (eq_dec_points A Y0).
+assert (A <> Y0).
  {
-  treat_equalities.
+  intro.
+  subst.
   unfold Definitions.BetS in *;spliter.
-  assert_diffs.
-  unfold Midpoint in *;spliter.
-  assert (HCong: tarski_axioms.Cong C0 X0 C0 X) by eCong.
-  assert (HCong2: tarski_axioms.Cong C0 Y C0 X) by eCong.
-  assert (eq:= between_cong C0 X Y H HCong2).
-  subst;intuition.  
+  assert (HCong: tarski_axioms.Cong C0 Y C0 X) by CongR.
+  assert (eq:= between_cong C0 X Y H HCong).
+  contradiction.
  }
 destruct HX0 as [HA [ HB [HC | HC]]].
 exists X0.
 exists Y0.
 assert_diffs.
-split;unfold Definitions.BetS;finish.
+split;unfold Definitions.BetS;Between.
 split.
-eCong.
+apply cong_transitivity with C0 X0; Cong.
 split.
-eCong.
-assert_bets;finish.
-split;finish.
-eBetween.
+Cong.
+split;auto.
+assert_bets; eBetween.
 
 assert (Lt C0 Y C0 X)
  by (unfold Definitions.BetS in *;spliter;apply (bet__lt1213);auto).
-assert (Le C0 X0 C0 A) 
+assert (Le C0 X0 C0 A)
  by (apply (bet__le1213);auto).
-assert (HCong: tarski_axioms.Cong C0 X0 C0 X) by eCong.
+assert (HCong: tarski_axioms.Cong C0 X0 C0 X) by CongR.
 assert (Lt C0 X C0 A)
  by (apply (cong2_lt__lt C0 X0 C0 A);finish).
 assert (Lt C0 Y C0 A)
  by (apply (le3456_lt__lt) with C0 X;auto using lt__le).
-assert (Hc : tarski_axioms.Cong C0 Y C0 A) by finish.
+assert (Hc : tarski_axioms.Cong C0 Y C0 A) by Cong.
 apply cong__nlt in Hc.
 intuition.
 Qed.
@@ -557,7 +543,7 @@ exists Q.
 exists R.
 exists P.
 unfold euclidean_axioms.CI;simpl;unfold CI.
-finish.
+auto.
 Qed.
 
 Lemma eOnCirc_OnCirc : forall A K, euclidean_axioms.OnCirc A K -> OnCirc A K.
@@ -571,7 +557,7 @@ destruct H as [X [Y [U HXY]]].
 spliter.
 unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *.
 spliter.
-inversion H;subst;finish.
+inversion H;subst; auto.
 Qed.
 
 Lemma eOutCirc_OutCirc : forall A K, euclidean_axioms.OutCirc A K -> OutCirc A K.
@@ -585,7 +571,7 @@ spliter.
 unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *.
 spliter.
 inversion H;subst.
-exists X;finish.
+exists X;auto.
 Qed.
 
 
@@ -601,7 +587,7 @@ destruct (symmetric_point_construction W V) as [Y HY].
 exists Y.
 assert_diffs.
 unfold Definitions.BetS.
-split;finish.
+repeat split;Between;Cong.
 destruct (segment_construction_3 U V V W H0 H) as [X HX].
 destruct (symmetric_point_construction X U) as [Y HY].
 spliter.
@@ -609,12 +595,12 @@ assert_diffs.
 assert_bets.
 exists X. exists Y.
 split.
-unfold Definitions.BetS;finish.
+unfold Definitions.BetS;auto.
 split.
-eCong.
+apply cong_transitivity with U X; Cong.
 split.
-eCong.
-unfold Definitions.BetS;finish.
+Cong.
+unfold Definitions.BetS;auto.
 Qed.
 
 Global Instance Euclid_neutral_ruler_compass_follows_from_Tarski_ruler_and_compass :
@@ -642,14 +628,14 @@ split.
   assumption.
   split.
   assumption.
-  unfold Definitions.BetS in *;spliter;finish.
+  unfold Definitions.BetS in *;spliter;auto.
 
-  
+
   split.
   destruct K as [p q].
   destruct p.
   apply OnCirc_OnCirc.
-  { 
+  {
    unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *.
    spliter.
    inversion H;subst;auto.
@@ -665,7 +651,7 @@ split.
    inversion H;subst;auto.
   }
   assumption.
-  
+
   unfold BetS;simpl;auto.
 
  exists Y.
@@ -677,18 +663,17 @@ split.
  unfold Definitions.Col in H2.
  destruct H2.
  unfold BetS;simpl;unfold Definitions.BetS.
- split;finish.
+ split;auto.
  destruct H2.
  exfalso;apply H7.
- eBetween.
- unfold  BetS;simpl;unfold Definitions.BetS.
- split;finish.
-  exfalso;apply H7;eBetween.
+ apply outer_transitivity_between2 with X; Between.
+ exfalso;apply H7.
+ apply (between_exchange3 X); auto.
  split.
   destruct K as [p q].
   destruct p.
   apply OnCirc_OnCirc.
-  { 
+  {
    unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *.
    spliter.
    inversion H;subst;auto.
@@ -698,13 +683,13 @@ split.
     destruct K as [p q].
   destruct p.
   apply OnCirc_OnCirc.
-  { 
+  {
    unfold euclidean_axioms.CI in *;simpl in *;unfold CI in *.
    spliter.
    inversion H;subst;auto.
   }
   assumption.
-  unfold BetS;simpl;unfold Definitions.BetS in *;spliter;finish.
+  unfold BetS;simpl;unfold Definitions.BetS in *;spliter;Between.
 
 - simpl.
   intros.
@@ -744,7 +729,7 @@ Lemma Euclid5 :
     exists X, BetS p a X /\ BetS s q X.
 Proof.
 intros.
-assert (T:tarski_s_parallel_postulate -> 
+assert (T:tarski_s_parallel_postulate ->
         euclid_5).
 {
  assert (T:=equivalent_postulates_without_decidability_of_intersection_of_lines_bis).
@@ -760,11 +745,11 @@ assert (T3:exists I : Tpoint, Definitions.BetS s q I /\ Definitions.BetS p a I).
 apply (T2 p q r s t a);
 auto using BetS_BetS.
 unfold BetS in *;simpl in *;
-unfold Definitions.BetS in *;spliter;finish.
+unfold Definitions.BetS in *;spliter;Between.
 intro HnCol.
 apply Col_Col in HnCol.
 apply nCol_not_Col in H4;intuition.
-finish.
+Cong.
 destruct T3 as [X HX].
 exists X;spliter;auto.
 Qed.

commit c3d7b81f19a0e898baeb3398b5854aaad89146c2

Fix `Export` problems (again)
Reliance on https://github.com/coq/coq/issues/10474 was re-introduced by d91694bcaea6df45f44b2a7a5884a6cedad802cf

Meta_theory/Models/tarski_to_euclid.v
@@ -4,6 +4,7 @@ Require Import GeoCoq.Axioms.continuity_axioms.
 Require Import GeoCoq.Meta_theory.Continuity.elementary_continuity_props.
 Require Import GeoCoq.Meta_theory.Parallel_postulates.parallel_postulates.
 
+Import circles.
 Import euclidean_axioms.
 
 Section Tarski_neutral_to_Euclid_neutral.

Tarski_dev/Annexes/inscribed_angle.v
@@ -47,7 +47,7 @@ Lemma bet_suma__suma : forall A B C D E F G H I, G <> H -> H <> I ->
   Bet G H I -> SumA A B C B C A D E F -> SumA D E F C A B G H I.
 Proof.
   intros A B C D E F G H I HGH HHI HBet HSuma.
-  assert_diffs.
+  suma.assert_diffs.
   destruct (bet__trisuma A B C G H I) as [D' [E' [F' []]]]; auto.
   apply (conga3_suma__suma D' E' F' C A B G H I); try apply conga_refl; auto.
   apply (suma2__conga A B C B C A); assumption.
@@ -56,7 +56,7 @@ Qed.
 Lemma suma__suppa : forall A B C D E F, SumA A B C B C A D E F -> SuppA D E F C A B.
 Proof.
   intros A B C D E F HSuma.
-  assert_diffs.
+  suma.assert_diffs.
   destruct (point_construction_different A B) as [A' []].
   apply bet_suma__suppa with A B A'; trivial.
   apply bet_suma__suma; auto.
@@ -98,7 +98,7 @@ Proof.
   intros A B C O HAC HBC HPer HCong1 HCong2 HCop.
   destruct (midpoint_existence A B) as [M HM].
   assert (M = O); [|subst; apply HM].
-  assert_diffs.
+  suma.assert_diffs.
   apply (cong4_cop2__eq A C B); Cong; [|Cop..].
   apply cong_commutativity, thales_converse_theorem with B; assumption.
 Qed.
@@ -109,7 +109,7 @@ Proof.
   intros A B C B' HAB HBC HAB' HBet HCong.
   apply ghalfa_chara; split.
     apply cong__acute; auto.
-  assert_diffs.
+  suma.assert_diffs.
   apply (conga3_suma__suma A B C B C A C A B'); try apply conga_refl; auto.
     apply high_school_exterior_angle_theorem; auto.
   apply conga_left_comm, l11_44_1_a; Cong.
@@ -129,7 +129,7 @@ Proof.
   { assert (HNCol1 : ~ Col A B O) by (eapply one_side_not_col123, HOS).
     assert (M <> O) by (intro; treat_equalities; apply HNCol1; Col).
     assert (O <> C) by (intro; treat_equalities; auto).
-    assert_diffs.
+    suma.assert_diffs.
     assert (Cong O A O C) by (apply (onc2__cong O P); assumption).
     destruct (angle_partition M O C); auto.
     - assert (HMO := H).
@@ -154,7 +154,7 @@ Proof.
       }
       assert (M <> H) by (intro; subst; apply one_side_not_col124 in HOS1; apply HOS1; Col).
       assert (Per M H C) by (apply perp_per_1, perp_left_comm, perp_col with O; Col).
-      apply lt_transitivity with H C; [|assert_diffs; apply l11_46; auto].
+      apply lt_transitivity with H C; [|suma.assert_diffs; apply l11_46; auto].
       apply cong_lt_per2__lt_1 with O O; Cong.
         apply l8_2, per_col with M; Col; Perp.
         apply perp_per_1, perp_left_comm, perp_col1 with B; Col.
@@ -170,7 +170,7 @@ Proof.
   destruct HLt as [[C' [HBet HCong]] HNCong].
   exists A, C', B; split.
     apply thales_theorem with M; trivial.
-  assert_diffs.
+  suma.assert_diffs.
   assert (C <> C') by (intro; subst; apply HNCong, HCong).
   apply os3__lta.
   - apply one_side_transitivity with M.
@@ -189,7 +189,7 @@ Lemma inscribed_angle_aux : forall O P A B C,
 Proof.
   intros O P A B C HA HB HC HOS HTS.
   destruct (segment_construction C O O P) as [C' []].
-  assert_diffs.
+  suma.assert_diffs.
   assert (O <> C') by (intro; treat_equalities; auto).
   assert (HCong := (onc2__cong O P)).
   apply suma_preserves_ghalfa with A C C' C' C B A O C' C' O B.
@@ -211,7 +211,7 @@ Proof.
   OS A B O C -> OS O C A B -> OS O B A C -> gHalfA A C B A O B).
   { intros O P A B C HA HB HC HOS1 HOS2 HOS3.
     destruct (chord_completion O P C O) as [C' [HC' HBet ]]; Circle.
-    assert_diffs.
+    suma.assert_diffs.
     assert (C' <> O) by (intro; treat_equalities; auto).
     assert (TS O B A C').
     { apply l9_8_2 with C; [|Side].
@@ -257,7 +257,7 @@ Proof.
   intros O P A B C HA HB HC HOS.
   assert (HCong := (onc2__cong O P)).
   destruct (col_dec A O C).
-  { assert_diffs.
+  { suma.assert_diffs.
     assert (Bet C O A) by (apply col_inc_onc2__bet with O P; Col; Circle).
     assert (O <> C) by (intro; treat_equalities; auto).
     apply ghalfa_right_comm, ghalfa_out4__ghalfa with O B B A; try apply out_trivial; auto.
@@ -265,7 +265,7 @@ Proof.
     apply bet_cong__ghalfa; auto.
   }
   destruct (col_dec B O C).
-  { assert_diffs.
+  { suma.assert_diffs.
     assert (Bet C O B) by (apply col_inc_onc2__bet with O P; Col; Circle).
     assert (O <> C) by (intro; treat_equalities; auto).
     apply ghalfa_left_comm, ghalfa_out4__ghalfa with O A A B; try apply out_trivial; auto.
@@ -282,7 +282,7 @@ Lemma diam_onc2_ts__suppa : forall O P A B C C',
   SuppA A C B A C' B.
 Proof.
   intros O P A B C C' HA HB [HBet [HC HC']] HTS.
-  assert_diffs.
+  suma.assert_diffs.
   assert (HCong := onc2__cong O P).
   assert (HMid : Midpoint O C C') by (split; Cong).
   assert (C <> C') by (intro; treat_equalities; auto).
@@ -292,31 +292,31 @@ Proof.
   assert (HSumaB : SumA B C C' C C' B C B C') by (apply cong_mid__suma with O; auto).
   assert (Per C A C') by (apply thales_theorem with O; auto).
   assert (Per C B C') by (apply thales_theorem with O; auto).
-  assert (HSuma : SumA C A C' C B C' C O C') by (assert_diffs; apply bet_per2__suma; auto).
+  assert (HSuma : SumA C A C' C B C' C O C') by (suma.assert_diffs; apply bet_per2__suma; auto).
   apply bet_suma__suppa with C O C'; trivial.
   destruct (ex_suma C A C' C' C B) as [D [E [F HSuma1]]]; auto.
   assert (HTS2 : TS C C' A B) by (apply (chord_intersection O P); assumption).
   assert (HTS3 : TS C' C A B) by (apply invert_two_sides, HTS2).
   assert (Acute C' C A).
-  { assert_diffs; apply acute_out2__acute with O A.
+  { suma.assert_diffs; apply acute_out2__acute with O A.
       apply l6_6, bet_out; auto.
       apply out_trivial; auto.
       apply cong__acute; auto.
   }
   assert (Acute C' C B).
-  { assert_diffs; apply acute_out2__acute with O B.
+  { suma.assert_diffs; apply acute_out2__acute with O B.
       apply l6_6, bet_out; auto.
       apply out_trivial; auto.
       apply cong__acute; auto.
   }
   assert (Acute C C' A).
-  { assert_diffs; apply acute_out2__acute with O A.
+  { suma.assert_diffs; apply acute_out2__acute with O A.
       apply l6_6, bet_out; Between.
       apply out_trivial; auto.
       apply cong__acute; auto.
   }
   assert (Acute C C' B).
-  { assert_diffs; apply acute_out2__acute with O B.
+  { suma.assert_diffs; apply acute_out2__acute with O B.
       apply l6_6, bet_out; Between.
       apply out_trivial; auto.
       apply cong__acute; auto.
@@ -340,7 +340,7 @@ Proof.
   destruct (col_dec A B O).
   { assert (Midpoint O A B) by (apply col_onc2__mid with P; auto).
     assert (Per A C B) by (apply thales_theorem with O; auto; apply cong_transitivity with O P; Cong).
-    assert_diffs; apply bet_per2__suma; Between.
+    suma.assert_diffs; apply bet_per2__suma; Between.
   }
   destruct (cop__one_or_two_sides A B O C); Col; Cop.
   - destruct (chord_completion O P C O) as [C' []]; Circle.
@@ -393,12 +393,12 @@ Proof.
   intros O P A B C C' HA HB HC HC' HOS HCop.
   assert_ncols.
   destruct (col_dec A B O).
-  { assert_diffs.
+  { suma.assert_diffs.
     assert (Midpoint O A B) by (apply col_onc2__mid with P; auto).
     apply l11_16; auto; apply thales_theorem with O; Col; apply cong_transitivity with O P; Cong.
   }
   destruct (cop__one_or_two_sides A B O C); Col; Cop.
-  - assert_diffs; destruct (cop2_onc4__or_conga_suppa O P A B C C') as [|Habs]; auto.
+  - suma.assert_diffs; destruct (cop2_onc4__or_conga_suppa O P A B C C') as [|Habs]; auto.
       apply coplanar_trans_1 with C; Col; Cop.
     exfalso.
     apply (nlta A C' B), acute_obtuse__lta.
@@ -421,7 +421,7 @@ Proof.
   assert (Haux : forall C C', OnCircle C O P -> OnCircle C' O P -> TS A B C C' -> OS A B O C ->
     SuppA A C B A C' B).
   { intros C C' HC HC' HTS HOS.
-    assert_diffs.
+    suma.assert_diffs.
     assert (~ Col C A B) by (destruct HTS; assumption).
     assert (Coplanar A B C' O) by (apply coplanar_trans_1 with C; Cop).
     destruct (cop2_onc4__or_conga_suppa O P A B C C') as [Habs|]; Cop.
@@ -435,7 +435,7 @@ Proof.
   intros C C' HC HC' HTS HCop.
   assert (~ Col C A B) by (destruct HTS; assumption).
   destruct (col_dec A B O).
-  { assert_diffs.
+  { suma.assert_diffs.
     assert (Midpoint O A B) by (apply col_onc2__mid with P; auto).
     destruct HTS as [_ []].
     apply per2__suppa; auto; apply thales_theorem with O; trivial; apply onc2__cong with P; assumption.
@@ -452,7 +452,7 @@ Lemma acute_cop_onc3__os : forall O P A B C, A <> B ->
   OS A B O C.
 Proof.
   intros O P A B C HAB HA HB HC HCop HAcute.
-  assert_diffs.
+  suma.assert_diffs.
   assert (~ Col A B C) by (apply (onc3__ncol O P); auto).
   apply coplanar_perm_1 in HCop.
   apply cop_nts__os; Col; intro Habs; apply (nlta A C B).
@@ -472,7 +472,7 @@ Lemma cop_obtuse_onc3__ts : forall O P A B C,
   TS A B O C.
 Proof.
   intros O P A B C HA HB HC HCop HObtuse.
-  assert_diffs.
+  suma.assert_diffs.
   assert (~ Col A B C) by (apply (onc3__ncol O P); auto).
   apply coplanar_perm_1 in HCop.
   apply cop_nos__ts; Col; intro Habs; apply (nlta A C B).
@@ -493,7 +493,7 @@ Lemma conga_cop2_onc4__os : forall O P A B C D, ~ Col A B O ->
   OS A B C D.
 Proof.
   intros O P A B C D HNCol HA HB HC HD HCopC HCopD HConga.
-  assert_diffs.
+  suma.assert_diffs.
   destruct (angle_partition A C B) as [HAcute|[HPer|HObtuse]]; auto.
   - apply one_side_transitivity with O; [apply one_side_symmetry|];
       apply acute_cop_onc3__os with P; Cop.
@@ -515,7 +515,7 @@ Lemma cop2_onc4_suppa__ts : forall O P A B C D, ~ Col A B O ->
   TS A B C D.
 Proof.
   intros O P A B C D HNCol HA HB HC HD HCopC HCopD HSuppa.
-  assert_diffs.
+  suma.assert_diffs.
   destruct (angle_partition A C B) as [HAcute|[HPer|HObtuse]]; auto.
   - apply l9_8_2 with O.
       apply cop_obtuse_onc3__ts with P; Cop; apply (acute_suppa__obtuse A C B); assumption.
@@ -554,7 +554,7 @@ Proof.
   assert (HCong := onc2__cong O P).
   assert (HCong' := onc2__cong O' P').
   destruct (col_dec A B O) as [|HNCol1].
-  { assert_diffs.
+  { suma.assert_diffs.
     assert (Midpoint O A B) by (apply col_onc2__mid with P; assumption).
     apply (l7_17 A B); trivial.
     apply thales_converse_theorem_1 with D; auto.
@@ -564,7 +564,7 @@ Proof.
   assert (HNCol' : ~ Col A B D) by (apply one_side_not_col124 with C, HOS).
   destruct (midpoint_existence A B) as [M HM].
   assert (HOS1 : OS A B O O').
-  { assert_diffs; destruct (angle_partition A C B) as [HAcute|[HPer|HObtuse]]; auto.
+  { suma.assert_diffs; destruct (angle_partition A C B) as [HAcute|[HPer|HObtuse]]; auto.
     - apply one_side_transitivity with C;
         [|apply one_side_transitivity with D; trivial; apply one_side_symmetry].
         apply acute_cop_onc3__os with P; auto.
@@ -579,8 +579,8 @@ Proof.
       apply (conga_obtuse__obtuse A C B); assumption.
   }
   assert (HNCol1' : ~ Col A B O') by (apply one_side_not_col124 with O, HOS1).
-  destruct (bet_cop_onc2__ex_onc_os_out O P A B C M) as [C1]; Between; Col; [assert_diffs; auto..|].
-  destruct (bet_cop_onc2__ex_onc_os_out O' P' A B D M) as [D1]; Between; Col; [assert_diffs; auto..|].
+  destruct (bet_cop_onc2__ex_onc_os_out O P A B C M) as [C1]; Between; Col; [suma.assert_diffs; auto..|].
+  destruct (bet_cop_onc2__ex_onc_os_out O' P' A B D M) as [D1]; Between; Col; [suma.assert_diffs; auto..|].
   spliter.
   assert (HNCol2 : ~ Col A B C1) by (apply one_side_not_col124 with C; assumption).
   assert (HOut : Out M C1 D1).
@@ -589,7 +589,7 @@ Proof.
     assert (O <> M) by (intro; subst; apply HNCol1; Col).
     assert (O' <> M) by (intro; subst; apply HNCol1'; Col).
     assert (Col O O' M); [|ColR].
-    assert_diffs; apply (cop_per2__col A); auto;
+    suma.assert_diffs; apply (cop_per2__col A); auto;
       [|apply mid_onc2__per with P B; auto|apply mid_onc2__per with P' B; auto].
     apply coplanar_trans_1 with B; Col; [|Cop].
     apply coplanar_trans_1 with C; Col; [Cop|].
@@ -597,14 +597,14 @@ Proof.
   }
   destruct (eq_dec_points C1 D1).
   { subst D1.
-    assert_diffs.
+    suma.assert_diffs.
     apply (cong4_cop2__eq A B C1); Cong; exists M; left; split; Col.
   }
   assert (HNCol2' : ~ Col A B D1) by (apply one_side_not_col124 with D; assumption).
   assert (CongA A C1 B A C B) by (apply (cop_onc4_os__conga O P); Side; exists M; left; split; Col).
   assert (CongA A D1 B A D B) by (apply (cop_onc4_os__conga O' P'); Side; exists M; left; split; Col).
-  assert (Out A B M) by (assert_diffs; apply l6_6, bet_out; Between).
-  assert (Out B A M) by (assert_diffs; apply l6_6, bet_out; Between).
+  assert (Out A B M) by (suma.assert_diffs; apply l6_6, bet_out; Between).
+  assert (Out B A M) by (suma.assert_diffs; apply l6_6, bet_out; Between).
   assert (HH := HOut).
   destruct HH as [HMC1 [HMD1 [HBet|HBet]]]; exfalso.
   - apply (lta_not_conga A D B A C B); CongA.
@@ -687,7 +687,7 @@ Proof.
   }
   intros C D HTS HSuppa.
   assert (HCop : Coplanar A B C D) by (apply ts__coplanar, HTS).
-  assert_diffs; destruct (angle_partition A C B) as [|[|]]; auto; split; trivial.
+  suma.assert_diffs; destruct (angle_partition A C B) as [|[|]]; auto; split; trivial.
   { destruct (Haux D C) as [_ [O [P]]].
       Side.
       apply (acute_suppa__obtuse A C B); trivial.
@@ -729,7 +729,7 @@ Lemma chord_par_diam : forall O P A B C C' A' U,
  Col A B U -> Perp O U A B -> Par A C' O U -> B = C.
 Proof.
 intros.
-assert_diffs.
+suma.assert_diffs.
 assert(Midpoint U A B).
 {
   apply(col_onc2_perp__mid O P A B U); Col.

Tarski_dev/Ch10_line_reflexivity_2.v
@@ -1,5 +1,5 @@
-Require Export GeoCoq.Tarski_dev.Ch10_line_reflexivity.
 Require Import GeoCoq.Meta_theory.Dimension_axioms.upper_dim_2.
+Require Export GeoCoq.Tarski_dev.Ch10_line_reflexivity.
 
 Section T10_1.
 
@@ -870,7 +870,6 @@ destruct (hilbert_s_version_of_pasch_aux C A B Q P) as [X [HPQX HBetS]]; Cop.
 exists X; split; Col; unfold BetS.
 induction HBetS; spliter; repeat split; Between.
 Qed.
-
 
 Lemma two_sides_cases : forall O P A B,
  ~ Col O A B -> OS O P A B -> TS O A P B \/ TS O B P A.

Tarski_dev/Ch11_angles.v
@@ -1,4 +1,5 @@
 Require Export GeoCoq.Tarski_dev.Ch10_line_reflexivity_2.
+Import Ch10_line_reflexivity.
 
 Section T11_1.
 