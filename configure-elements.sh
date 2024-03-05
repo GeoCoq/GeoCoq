195f80f40e9536a39ad99b7fb67ab793fcb2cfb4
Pierre Boutry <contact@pierre-boutry.fr>
2024-03-05 14:17:49 +0100

    update scripts (need to do so for pof and modify main accordingly)

--- configure-elements.sh
+++ configure-elements.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-cp -f _CoqProject.in _CoqProject
-find . -name "*.v" | grep OriginalProofs >> _CoqProject
+cp -f _CoqProject-Elements.in _CoqProject
+find . -name "*.v" | grep OriginalProofs   >> _CoqProject
 find . -name "*.v" | grep euclid_to_tarski >> _CoqProject
 coq_makefile -f _CoqProject -o Makefile
 rm -f .coqdeps.d
