47ee623ef26a694c8e551b4c870ba5bbd6de604c
Pierre Boutry <contact@pierre-boutry.fr>
2024-03-10 15:15:51 +0100

    add missing dependency in main.v

--- main.v
+++ main.v
@@ -4,6 +4,7 @@
 Require Export GeoCoq.Highschool.incenter.
 Require Export GeoCoq.Highschool.orientation.
 Require Export GeoCoq.Highschool.Euler_line.
+Require Export GeoCoq.Highschool.SegmTrisect.
 Require Export GeoCoq.Highschool.sesamath_exercises.
 Require Export GeoCoq.Elements.Statements.Book_1.
 Require Export GeoCoq.Elements.Statements.Book_3.
